28da3c8371541a21bee29d370fdd0a13ea215352
gab3-dev <gabe.dev@icloud.com>
2022-09-09 18:42:26 -0300

    Initial

---
+++ lib/features/landing/screens/landing_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/common/utils/colors.dart';
+import 'package:whatsapp_ui/common/widgets/custom_button.dart';
+import 'package:whatsapp_ui/features/auth/screens/login_screen.dart';
+import 'package:whatsapp_ui/features/auth/screens/signup_screen.dart';
+
+class LandingScreen extends StatelessWidget {
+  const LandingScreen({Key? key}) : super(key: key);
+
+  void navigateToLoginScreen(BuildContext context) {
+    Navigator.pushNamed(context, LoginScreen.routeName);
+  }
+
+  void navigateToSignUpScreen(BuildContext context) {
+    Navigator.pushNamed(context, SignUpScreen.routeName);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 50),
+            const Text(
+              'Welcome to WhatsApp',
+              style: TextStyle(
+                fontSize: 33,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(height: size.height / 9),
+            Image.asset(
+              'assets/bg.png',
+              height: 340,
+              width: 340,
+              color: tabColor,
+            ),
+            SizedBox(height: size.height / 9),
+            const Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                'Read our Privacy Policy. Tap "Agree and continue" to accept the Terms of Service.',
+                style: TextStyle(color: greyColor),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: SizedBox(
+                child: CustomButton(
+                  text: 'AGREE AND CONTINUE',
+                  onPressed: () => navigateToLoginScreen(context),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: SizedBox(
+                child: CustomButton(
+                  text: 'CREATE NEW ACCOUNT',
+                  onPressed: () => navigateToSignUpScreen(context),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
